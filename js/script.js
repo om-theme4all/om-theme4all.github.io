5464529bbf169db9cae14d710202bfd94c847c63
Houssam Hammouda <devhoussam123@gmail.com>
2018-11-18 11:42:42 -0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*global $*/
+
+$(function () {
+    
+    "use strict";
+    
+/*==================================
+    * Author        : Houssam Hammouda
+    * Template Name : om-theme4all - om-theme4all.github.io
+==================================== */
+    
+    
+});
